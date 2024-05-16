e68e35063c7df7c7f7351f32db3dd10a2db18cff
FiereEinar <nickxylanmelloria@gmail.com>
2024-05-16 09:47:22 +0800

    functionality finished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+router.get('/', function (req, res, next) {
+  res.render('home', { title: 'Messages', messages: messages });
 });
+
+router.get('/new', function (req, res, next) {
+  res.render('form');
+});
+
+router.post('/new', function (req, res, next) {
+  const author = req.body.name
+  const message = req.body.message
+
+  if (!author || !message) {
+    return res.status(400).send("Name and message are required");
+  }
+
+  messages.push({ text: message, user: author, added: new Date() });
+
+  res.redirect('/')
+})
 
 module.exports = router;
